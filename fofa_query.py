882ae78ae7a17ef532165b53652b01467829e689
zhangzd <13691994063@163.com>
2019-03-12 01:33:18 -0700

    save_mysql

--- fofa_query.py
+++ fofa_query.py
@@ -3,18 +3,44 @@
 __author__ = "zhangzd"
 
 import fofa
-from settings import KEY, EMAIL
+import pymysql
+from settings import KEY, EMAIL, mysql_conn
 import sys
+
+
+class Save_data:
+    def __init__(self, mysql_conn):
+        self.db = pymysql.connect(**mysql_conn)
+        self.cursor = self.db.cursor()
+
+    def save(self, sql):
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except:
+            self.db.rollback()
+
+    def close(self):
+        self.db.close()
+
 
 class Fofa:
     def __init__(self, email, key, query_str ):
         self.client = fofa.Client(email, key)
-        self.query_str = 'domain="{}"'.format(query_str)
+        self.query_str = 'domain="{}" && after="2015" && before="2019-10-01"'.format(query_str)
+
     def get_query_result(self):
+        save_data = Save_data(mysql_conn)
         for page in range(1, 101):
-            data = self.client.get_data(self.query_str, page=page, fields="ip, port, country, city, host, title")
-            for ip, port, country, city, host, title in data["results"]:
-                print("ip:%s port:%s country:%s city:%s host:%s title:%s" % (ip, port, country, city, host, title))
+            data = self.client.get_data(self.query_str, page=page, fields="host, title, ip, domain, port, country, city")
+            for host, title, ip, domain, port, country, city in data["results"]:
+               # print("ip:%s port:%s country:%s city:%s host:%s title:%s" % (ip, port, country, city, host, title))
+                sql = "insert into fofa(host, title, ip, domain, port, country, city) values\
+                 ('%s', '%s', '%s', '%s', '%s', '%s', '%s')"%\
+                 (host, title, ip, domain, port, country, city)
+                save_data.save(sql)
+        save_data.close()
+
 
 if __name__ == "__main__":
     try:
--- settings.py
+++ settings.py
@@ -1,2 +1,10 @@
 KEY = "534543fb332c5375515cae50ff60fa8a"
 EMAIL = "houzan@hotmail.com"
+mysql_conn = {
+    "host": "192.168.50.61",
+    "user": "root",
+    "password": "RONGhe@123",
+    "database": "message_repository",
+    "port": 3306,
+    "charset": "utf8"
+}
